3325d29955080fd202b8a7517d6a2f0b79e393d5
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-07-28 13:37:39 +0530

    [Tushar Vidwans] Adding all the business logic for registration and login

---
+++ BusinessLayer/Interface/IUserBL.cs
@@ -0,0 +1,17 @@
+﻿using CommonLayer.Model;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Interface
+{
+    public interface IUserBL
+    {
+
+        public UserEntity Registration(UserRegistrationModel userRegistrationModel);
+
+        public string Login(UserLogin userlogin);
+
+    }
+}
---
+++ BusinessLayer/Service/UserBL.cs
@@ -0,0 +1,55 @@
+﻿using BusinessLayer.Interface;
+using CommonLayer.Model;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Service
+{
+    public class UserBL : IUserBL
+    {
+
+        private readonly IUserRL iuserRL;
+
+        public UserBL(IUserRL iuserRL)
+        {
+            this.iuserRL = iuserRL;
+        }
+
+        public UserEntity Registration(UserRegistrationModel userRegistrationModel)
+        {
+
+            try
+            {
+
+                return iuserRL.Registration(userRegistrationModel);
+
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+
+        }
+
+        public string Login(UserLogin userlogin)
+        {
+
+            try
+            {
+
+                return iuserRL.Login(userlogin);
+
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
